8ed610e5136fb53f0612253744d9cc0415332ed2
Brendayy <reginabrendayy@gmail.com>
2023-06-06 03:27:21 -0400

    initial commit

---
+++ asr_eval/asr_eval.py
@@ -0,0 +1,62 @@
+from torchmetrics import WordErrorRate
+import json
+import click
+import mmif
+from mmif import Mmif
+
+
+def get_text_from_mmif(mmif): 
+  with open(mmif, 'r') as f:
+    data = json.load(f)
+
+  # view is a list with only one dict
+  view_dict = data['views'][0]
+  # annotation is a list of dictionaries
+  annotation_dict = view_dict['annotations'][0]
+  text = annotation_dict['properties']['text']['@value']
+
+  return text
+
+
+def get_text_from_txt(txt):
+  with open(txt, 'r') as f:
+    text = f.read()
+  return text
+
+
+  # for now, we only care about casing, more processing steps might be added in the future
+def process_text(text, ignore_case):
+  if ignore_case:
+    text = text.upper()
+  return text
+
+
+  # todo: using mmif sdk
+  # with open('/content/drive/MyDrive/output.mmif', 'r') as f:
+  # mmif_obj = Mmif(f.read())
+
+
+@click.command()
+@click.option("--hyp-file", type=click.Path(readable=True), required=True)
+@click.option("--gold-file", type=click.Path(readable=True), required=True)
+@click.option("--exact-case", is_flag=True)
+def main(hyp_file, gold_file, exact_case):
+  # argparse(f'')
+  # os.listdir or walk
+  # loop over two lists
+  wer = WordErrorRate()
+  # with open('/content/drive/MyDrive/output.mmif', 'r') as f:
+  #   data = json.load(f)
+
+  # if we want to ignore casing
+  hyp = process_text(get_text_from_mmif(hyp_file), not exact_case)
+  gold = process_text(get_text_from_txt(gold_file), not exact_case)
+
+  print(wer(hyp, gold).item())
+
+
+if __name__ == "__main__":
+  main()
+
+  
+

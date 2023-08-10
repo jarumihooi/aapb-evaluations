37ed95f74c5fc1642a8c6d6d2e13fc9c0afbb112
Wesley Ricketts <wricketts@brandeis.edu>
2023-08-10 16:36:58 -0400

    Add modifications to nel eval - all entities output by the system are retained for evaluation - gold entities without a KBID are still filtered out - replace accuracy metric with precision, recall, F1 - define NamedEntityLink class within a separate dependency nel.py - add unit tests for nel.py

--- nel_eval/evaluate.py
+++ nel_eval/evaluate.py
@@ -21,67 +21,8 @@ from lapps.discriminators import Uri
 from mmif.serialize import Mmif
 import os
 import pandas as pd
-from typing import List, Union
-from warnings import warn
 
-
-class NamedEntityLink:
-    def __init__(self, surface_form: str, uris: Union[List[str], str]) -> None:
-        """
-        Initializes NEL instance. Gold data provides the URI as a string, whereas system output MMIFs provide
-        the URI(s) in a list format.
-        :param surface_form: the text span corresponding to the entity.
-        :param uris: the Wikidata URI(s) grounding the entity.
-        """
-        self.surface_form = surface_form
-        if isinstance(uris, str):
-            if uris != '':
-                self.qid = uris.rsplit("/", 1)[1]
-            else:
-                self.qid = ''
-        elif isinstance(uris, list):
-            # multiple wikidata URIs
-            if len(uris) > 1:
-                self.qid = []
-                for uri in uris:
-                    self.qid.append(uri.rsplit("/", 1)[1])
-            # single URI
-            elif len(uris) == 1:
-                self.qid = uris[0].rsplit("/", 1)[1]
-            # in case URI is missing
-            else:
-                self.qid = ''
-                warn("NEL object initialized without a QID")
-        else:
-            raise TypeError(f"Argument uris must be of type List[str] or str, not {type(uris)}")
-
-    def __str__(self) -> str:
-        """Returns a printable string representation of the NEL instance."""
-        return f"{self.surface_form} (QID: {self.qid})"
-
-    def __eq__(self, other) -> bool:
-        """Returns True if another object is an NEL instance with the same surface form
-        and QID as this one. Returns False otherwise."""
-        if isinstance(other, NamedEntityLink):
-            if isinstance(self.qid, str):
-                if self.qid == other.qid and self.surface_form == other.surface_form:
-                    return True
-            elif isinstance(self.qid, list):
-                if other.qid in self.qid and self.surface_form == other.surface_form:
-                    return True
-        return False
-
-    def substring_match(self, other) -> bool:
-        """Returns True if the surface form of this instance is a substring of the surface form
-        of another NEL instance and both objects have the same QID."""
-        if isinstance(other, NamedEntityLink):
-            if isinstance(self.qid, str):
-                if self.qid == other.qid and self.surface_form in other.surface_form:
-                    return True
-            elif isinstance(self.qid, list):
-                if other.qid in self.qid and self.surface_form in other.surface_form:
-                    return True
-        return False
+from nel import NamedEntityLink
 
 
 def match_files(test_dir, gold_dir) -> list:
@@ -105,17 +46,13 @@ def match_files(test_dir, gold_dir) -> list:
     return file_matches
 
 
-def filter_common_entities(test_mmif, gold_tsv) -> (list, list):
-    """Return lists of the common NEL instances between system-generated mmif and gold data."""
-    test_entities, gold_entities = file_to_ne(test_mmif), file_to_ne(gold_tsv)
+def filter_nil_entities(gold_tsv) -> list:
+    """Returns list of gold entities excluding the ones without grounding."""
+    gold_entities = file_to_ne(gold_tsv)
     # remove gold NEL instances whose QIDs are empty strings
-    gold_entities = [gold_ent for gold_ent in gold_entities if gold_ent.qid != '']
-    # remove test NEL instances whose surface forms do not overlap with the gold NEL instances
-    gold_surface_forms = [gold_ent.surface_form for gold_ent in gold_entities]
-    test_entities = [test_ent for test_ent in test_entities
-                     if any(test_ent.surface_form in g for g in gold_surface_forms)]
+    gold_entities = [gold_ent for gold_ent in gold_entities if gold_ent.kbid]
 
-    return test_entities, gold_entities
+    return gold_entities
 
 
 def file_to_ne(file_path: str) -> list:
@@ -140,9 +77,11 @@ def mmif_to_ne(mmif_path) -> list:
     view = ne_views[-1]  # read only the first view (from last) with Uri.NE
     annotations = view.get_annotations(at_type=Uri.NE)
     ne_list = []
+    guid = os.path.basename(mmif_path)[:24]
     for anno in annotations:
         entity = anno.properties
-        ne = NamedEntityLink(entity['text'], entity['grounding'][1:])
+        ne = NamedEntityLink(guid, entity['start'], entity['end'], entity['category'], entity['text'],
+                             entity['grounding'][1:])
         ne_list.append(ne)
 
     return ne_list
@@ -154,7 +93,11 @@ def tsv_to_ne(gold_tsv_path) -> list:
     with open(gold_tsv_path) as fh_in:
         annotations_df = pd.read_csv(fh_in, sep='\t', encoding='utf-16')
         annotations_df.fillna('', inplace=True)
-    ne_list = [NamedEntityLink(ent, qid) for ent, qid in zip(annotations_df['entity'], annotations_df['QID'])]
+        annotations_df['guid'] = annotations_df['guid'].apply(lambda x: x[:24])
+    ne_list = [NamedEntityLink(guid, begin_offset, end_offset, ent_type, text, uris) for
+               guid, begin_offset, end_offset, ent_type, text, uris in
+               zip(annotations_df['guid'], annotations_df['begin_offset'], annotations_df['end_offset'],
+                   annotations_df['type'], annotations_df['text'], annotations_df['qid'])]
 
     return ne_list
 
@@ -162,38 +105,28 @@ def tsv_to_ne(gold_tsv_path) -> list:
 def evaluate(test_dir, gold_dir):
     results = defaultdict(dict)
     file_matches = match_files(test_dir, gold_dir)
-    for test_file, gold_file in file_matches:
-        print(f'>>> Evaluating {os.path.basename(test_file)}')
-        guid = os.path.basename(test_file)[:24]
+    for sys_file, gold_file in file_matches:
+        print(f'>>> Evaluating {os.path.basename(sys_file)}')
+        guid = os.path.basename(sys_file)[:24]
 
-        # calculate proportion of gold and test data actually used
-        unfiltered_test, unfiltered_gold = file_to_ne(test_file), file_to_ne(gold_file)
-        test_entities, gold_entities = filter_common_entities(test_file, gold_file)
-        used_gold = len(gold_entities) / len(unfiltered_gold)
-        used_test = len(test_entities) / len(unfiltered_test)
-        results[guid]['Gold Entities'] = {"count": len(gold_entities),
-                                          "proportion of total": "{:.2f}".format(used_gold)}
-        results[guid]['Test Entities'] = {"count": len(test_entities),
-                                          "proportion of total": "{:.2f}".format(used_test)}
+        sys_instances = frozenset(file_to_ne(sys_file))
+        gold_instances = frozenset(filter_nil_entities(gold_file))
+        results[guid]['Gold Entities'] = {"count": len(gold_instances)}
+        results[guid]['System Entities'] = {"count": len(sys_instances)}
 
-        # calculate accuracy
-        hits = 0
-        misses = 0
-        while test_entities:
-            test_ent = test_entities.pop(0)
-            found_match = False
-            for gold_ent in gold_entities:
-                if test_ent == gold_ent or test_ent.substring_match(gold_ent):
-                    hits += 1
-                    found_match = True
-                    gold_entities.remove(gold_ent)
-                    break
-            if not found_match:
-                misses += 1
-        acc = hits / (hits + misses)
+        # calculate precision
+        precision = len(gold_instances.intersection(sys_instances)) / len(sys_instances)
 
-        results[guid]['Accuracy'] = "{:.2f}".format(acc)
-        print('... done')
+        # calculate recall
+        recall = len(gold_instances.intersection(sys_instances)) / len(gold_instances)
+
+        # calculate F1
+        f1 = 2 * (precision * recall) / (precision + recall)
+
+        results[guid]['Precision'] = "{:.2f}".format(precision)
+        results[guid]['Recall'] = "{:.2f}".format(recall)
+        results[guid]['F1'] = "{:.2f}".format(f1)
+        print('>>> ... done')
 
     return results
 
@@ -201,17 +134,17 @@ def evaluate(test_dir, gold_dir):
 def write_results(data: dict, result_path: str):
     """Write evaluation results to txt file."""
     with open(result_path, 'w') as fh_out:
-        json.dump(data, fh_out, indent=4, sort_keys=True)
+        json.dump(data, fh_out, indent=4)
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(description='Evaluate accuracy of NEL mmif files against gold labeled data.')
-    ap.add_argument('test_directory', metavar='test-dir', nargs='?',
-                    help='directory containing test data in .mmif format.')
+    ap.add_argument('system_data_directory', metavar='sys-dir', nargs='?',
+                    help='directory containing system output data in .mmif format.')
     ap.add_argument('gold_directory', metavar='gold-dir', nargs='?',
                     help='directory containing gold data in .tsv format.')
     ap.add_argument('-o', '--output', nargs='?', help='path to print out eval result.', default='results.txt')
     args = ap.parse_args()
 
-    data = evaluate(args.test_directory, args.gold_directory)
+    data = evaluate(args.system_data_directory, args.gold_directory)
     write_results(data, args.output)

dfa0afd677dce545d2668d714a160edac86478ac
jarumihooi <jer.huey@gmail.com>
2024-01-02 16:36:32 -0800

    added prelim version of sample_eval module and eval module plus temp test files to be removed later

---
+++ WIP/sample_eval/sample_eval.py
@@ -0,0 +1,74 @@
+import argparse
+
+import typing
+import os
+import sys
+# Get the current file's directory (where this script is located)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Go up one directory to get the root directory
+root_dir = os.path.abspath(os.path.join(current_dir, '..'))
+root_dir = os.path.abspath(os.path.join(root_dir, '..'))
+# Add the root directory to sys.path
+sys.path.append(root_dir)
+# from eval_module import goldretriever
+import eval_module
+
+# import pandas as pd
+
+# from clams_utils.aapb import goldretriever
+# source ../clams_env/bin/activate
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # EVALUATION TASK/MODEL INFORMATION (Edit these to needed values.) ====
+    APPNAME = "parseqocr-wrapper"  # default app
+    APPVERSION = 1.0  # default version
+    GOLDS_DIR = 'https://github.com/clamsproject/aapb-annotations/tree/f884e10d0b9d4b1d68e294d83c6e838528d2c249/newshour-transcript-sync/golds/aapb-collaboration-21'
+
+    # Parse CLI ====
+    parser = argparse.ArgumentParser(description="This sample evaluation of <task> is written to test modularization of the evals utils.\n"
+                                                 "An example run is\n"
+                                                 "'py -m sample_eval -g /google.com -p eval_module/ -r resultssss.txt'")
+    parser.add_argument("-g", "--golds-dir", help="github commit url link to gold standard <filetype> data dir, if None will goldretrieve from aapb-annotations for evaluation standard.", default=None)
+    parser.add_argument("-p", "--preds-dir", help="path to local dir with machine-prediction <filetype> files to be evaluated.")
+    parser.add_argument("-r", "--res", help="filepath/name for output results from this evaluation", default="results.txt") #could add datetime of current run.
+    # parser.add_argument("-o", "--out-dir", help="path for a results dir", default=None) # add this line if an output dir is needed.
+    args = parser.parse_args(); # print(args)
+
+    golds_dir, preds_dir, out_dir = eval_module.locate_input.locate(args, APPNAME, APPVERSION, GOLDS_DIR)
+    # outdir = None.
+
+    # get individual entries from golds and preds =====
+    # get golds files
+    # get preds files
+    # collect out entries
+    # pair
+
+
+
+
+
+# ======
+# TODO:
+'''
+Progress: Got module and paths to work. 
+Created dummy reads. Now fix if the Path globs are correct. 
+Consider which eval I want to emulate. 
+Consider if we can move this get code to the eval_module. 
+'''
+
+'''
+# Importing modules from the package
+from my_package import module1
+from my_package.subpackage import sub_module
+
+# Using the imported modules
+module1.some_function()
+sub_module.another_function()
+'''
---
+++ eval_module/locate_input.py
@@ -0,0 +1,28 @@
+import argparse
+import goldretriever
+from pathlib import Path
+
+def dummy_read_golds(gold_dir):
+    print(gold_dir)
+    return gold_dir
+
+
+def dummy_read_preds_mmifs(preds_dir, gold_entries):
+    print(f"{preds_dir=}", f"{gold_entries}")
+    return preds_dir
+
+def locate(args, APPNAME, APPVERSION, GOLDS_DIR):
+    # sets the folder names/paths correctly, modify GOLDS_DIR for the correct link to the eval needed.
+    golds_dir = goldretriever.download_golds(GOLDS_DIR) if args.golds_dir is None else args.golds_dir
+    preds_dir = f"preds@{APPNAME}{APPVERSION}@batch2" if args.preds_dir is None else args.preds_dir
+    # out_dir = Path(args.output_dir) if args.output_dir else Path(f"results@{APPNAME}{APPVERSION}@batch2") # Add this line if an output dir is needed for results.
+    out_dir = None
+    if out_dir is not None and not out_dir.exists():
+        out_dir.mkdir()
+
+    golds_entries = dummy_read_golds(Path(golds_dir).glob("*.tsv"))
+    preds_entries = dummy_read_preds_mmifs(Path(preds_dir).glob("*.mmif"),
+                                           golds_entries)  # uses gold_entries to get GUIDS to get matching preds?
+
+    return golds_dir, preds_dir, out_dir
+
